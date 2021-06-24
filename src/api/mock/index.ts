f0dd0f9383850049ad610b3268569714ec83de88
Ilya Azin <martis.azin@gmail.com>
2021-06-25 01:06:54 +0300

    feat(mock-api): add and refine other routes

--- src/api/mock/index.ts
+++ src/api/mock/index.ts
@@ -1,16 +1,74 @@
-import { createServer } from 'miragejs';
+import { Model, createServer } from 'miragejs';
 
-import { cards } from './fixtures';
+import type { User } from '../types';
+import { cards, users, viewer } from './fixtures';
 
+/**
+ * Mock-api server for internal development
+ * @see https://cardbox.github.io/backend/api-internal/index.html
+ */
 export function runMockServer() {
-  const server = createServer({
+  const instance = createServer({
     environment: 'development',
+    models: {
+      card: Model,
+    },
+    seeds(server) {
+      server.db.loadData({ cards, users });
+    },
     routes() {
-      this.post('/cards.list', () => {
-        return cards;
+      this.post('/users.viewer', (schema) => {
+        return schema.db.users.find(viewer.id);
+      });
+
+      this.post('/cards.list', (schema) => {
+        return schema.db.cards;
+      });
+
+      this.post('/cards.get', (schema, request) => {
+        const payload = JSON.parse(request.requestBody);
+        const { cardId } = payload;
+        if (!cardId) return null;
+
+        return schema.db.cards.find(cardId);
+      });
+
+      this.post('/cards.create', (schema, request) => {
+        const payload = JSON.parse(request.requestBody);
+
+        return schema.db.cards.insert(payload);
+      });
+
+      this.post('/cards.edit', (schema, request) => {
+        const payload = JSON.parse(request.requestBody);
+        const { cardId, ...diffPayload } = payload;
+        if (!cardId || Object.keys(diffPayload).length === 0) return null;
+
+        return schema.db.cards.update(cardId, diffPayload);
+      });
+
+      // @ts-ignore // FIXME: fix later
+      this.post('/cards.delete', (schema, request) => {
+        const payload = JSON.parse(request.requestBody);
+        const { cardId } = payload;
+        if (!cardId) return null;
+
+        return schema.db.cards.remove(cardId);
+      });
+
+      this.post('/cards.save', (schema, request) => {
+        const payload = JSON.parse(request.requestBody);
+        const { cardId } = payload;
+        if (!cardId) return null;
+
+        // FIXME: refine
+        const viewerDb: User = schema.db.users.find(viewer.id);
+        viewerDb.favorites.push(cardId);
+
+        return schema.db.users.update(viewer.id, viewerDb);
       });
     },
   });
 
-  return server;
+  return instance;
 }
