4ff635843183ca2ad0fe6a4e314cd6743558e949
Ilya Azin <martis.azin@gmail.com>
2021-06-26 13:36:14 +0300

    style(lint): run eslint (clrf => lf)

--- src/entities/card/organisms/card-preview.tsx
+++ src/entities/card/organisms/card-preview.tsx
@@ -126,9 +126,6 @@ const AddButtonStyled = styled(button.Icon)<{
     }
   }
 `;
-const Header = styled.header`
-  display: flex;
-  justify-content: space-between;
 
 const Header = styled.header`
   display: flex;
