364b323e1c60fe2712fd062f8fcab4425453f054
Ilya Azin <martis.azin@gmail.com>
2021-07-15 02:05:18 +0300

    BOX-97: fix(card-edit): normalize fixtures for editor

--- src/api/mock/fixtures.ts
+++ src/api/mock/fixtures.ts
@@ -1,6 +1,12 @@
+import { getValueNode } from '@box/lib/editor';
+
 import type { Card, User } from '../types';
 
 const stubCards = (amount: number) => new Array(amount).fill('-1');
+
+// FIXME: temp hack
+const asCardContent = (content: string) =>
+  JSON.stringify(getValueNode(content));
 
 // NOTE: preudo "uuid" for id property
 export const userSova: User = {
@@ -144,7 +150,9 @@ export const cards: Card[] = [
   {
     id: '1',
     title: `Manage map or Set in effector store`,
-    content: `Sometimes we need to save Set in effector store. Simple createStore(new Set) will not trigger updates on.add(item). Sometimes we need to save Set in effector store. Simple createStore(new Set) will not trigger updates on.add(item). Sometimes we need to save Set in effector store. Simple createStore(new Set) will not trigger updates on.add(item)`,
+    content: asCardContent(
+      `Sometimes we need to save Set in effector store. Simple createStore(new Set) will not trigger updates on.add(item). Sometimes we need to save Set in effector store. Simple createStore(new Set) will not trigger updates on.add(item). Sometimes we need to save Set in effector store. Simple createStore(new Set) will not trigger updates on.add(item)`,
+    ),
     author: viewer,
     createdAt: '2021-01-03T05:03:00.000Z',
     updatedAt: '2021-01-03T12:34:00.000Z',
@@ -153,7 +161,9 @@ export const cards: Card[] = [
   {
     id: '2',
     title: `Effector: sample vs forward, Effector: sample vs forward`,
-    content: `Sample: This method can be used for linking two nodes, resulting the third one, which will fire only upon clock node trigger.\nForward: Method to create connection between units in a declarative way. Sends updates from one set of units to another`,
+    content: asCardContent(
+      `Sample: This method can be used for linking two nodes, resulting the third one, which will fire only upon clock node trigger.\nForward: Method to create connection between units in a declarative way. Sends updates from one set of units to another`,
+    ),
     author: viewer,
     createdAt: '2021-01-04T05:03:00.000Z',
     updatedAt: '2021-01-04T12:34:00.000Z',
@@ -162,7 +172,8 @@ export const cards: Card[] = [
   {
     id: '3',
     title: `Effects sequence, Effects sequence, Effects sequence`,
-    content: `We'll need it when second request to the server requires resolved data from the first one
+    content:
+      asCardContent(`We'll need it when second request to the server requires resolved data from the first one
     \`\`\`
     import ReactDOM from 'react-dom'
 
@@ -193,7 +204,7 @@ export const cards: Card[] = [
     \`\`\`
 
     - See: https://effector.dev/docs/introduction/examples#effects-sequence
-    `,
+    `),
     author: viewer,
     createdAt: '2021-01-05T05:03:00.000Z',
     updatedAt: '2021-01-05T12:34:00.000Z',
@@ -202,9 +213,10 @@ export const cards: Card[] = [
   {
     id: '4',
     title: `Effector: Domain usage`,
-    content: `Domain is a namespace for your events, stores and effects.
+    content:
+      asCardContent(`Domain is a namespace for your events, stores and effects.
     Domain can subscribe to event, effect, store or nested domain creation with onCreateEvent, onCreateStore, onCreateEffect, onCreateDomain methods.
-    It is useful for logging or other side effects.`,
+    It is useful for logging or other side effects.`),
     author: viewer,
     createdAt: '2021-01-06T05:03:00.000Z',
     updatedAt: '2021-01-06T12:34:00.000Z',
@@ -213,9 +225,10 @@ export const cards: Card[] = [
   {
     id: '5',
     title: `Stop using Effector`,
-    content: `Effector is a brand new reactive state manager. Its ambitious team aims to solve all the problems that existing solutions have. Writing the core of the library from scratch took several attempts across six months, and recently the team released the first stable release.
+    content:
+      asCardContent(`Effector is a brand new reactive state manager. Its ambitious team aims to solve all the problems that existing solutions have. Writing the core of the library from scratch took several attempts across six months, and recently the team released the first stable release.
 
-    In this article, I will show why I prefer using Effector for my new projects instead of other state managers. Let's get started with the Effector API.`,
+    In this article, I will show why I prefer using Effector for my new projects instead of other state managers. Let's get started with the Effector API.`),
     author: viewer,
     createdAt: '2021-01-07T05:03:00.000Z',
     updatedAt: '2021-01-07T12:34:00.000Z',
