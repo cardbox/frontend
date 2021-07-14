2dcba6b41475742c1aaaee4ca327d1a778ca45e8
Ilya Azin <martis.azin@gmail.com>
2021-07-15 00:57:32 +0300

    BOX-97: feat(card-edit): integrate Editor basely

--- src/pages/card/edit/index.tsx
+++ src/pages/card/edit/index.tsx
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ContentCenteredTemplate } from '@box/ui';
+import { Editor } from '@cardbox/editor';
+import type { EditorValue } from '@cardbox/editor';
+import { getValueNode } from '@box/lib/editor';
 
 export const CardEditPage = () => {
-  return <ContentCenteredTemplate>EDIT PAGE</ContentCenteredTemplate>;
+  const [value, setValue] = useState<EditorValue>(getValueNode('Test'));
+
+  return (
+    <ContentCenteredTemplate>
+      <Editor value={value} onChange={setValue} />
+    </ContentCenteredTemplate>
+  );
 };
