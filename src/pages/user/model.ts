bc6f1c283ebf9f46a0b312f929c258cfe5fc6876
Ilya Azin <martis.azin@gmail.com>
2021-08-12 18:42:44 +0300

    BOX-185: @refactor(bind-api): refine pending for UserPage

--- src/pages/user/model.ts
+++ src/pages/user/model.ts
@@ -1,23 +1,33 @@
 import { StartParams } from '@box/lib/page-routing';
-import { attach, createEvent, restore, sample } from 'effector-root';
+import { attach, combine, createEvent, restore, sample } from 'effector-root';
 import { internalApi } from '@box/api';
 
 export const pageLoaded = createEvent<StartParams>();
 export const usersGetFx = attach({
   effect: internalApi.usersGet,
-  mapParams: (res: StartParams) => {
-    return {
-      body: {
-        username: res.params.username,
-      },
-    };
-  },
+});
+export const cardsListFx = attach({
+  effect: internalApi.cardsList,
 });
 
-export const $pagePending = restore(usersGetFx.pending.updates, true);
+export const $userPending = restore(usersGetFx.pending.updates, true);
+export const $cardsPending = restore(cardsListFx.pending.updates, true);
+
+// FIXME: Позже скорее всего разобъем SkeletonLayout
+// Но пока пусть вся страница будет дожидаться полного резолва эффектов
+export const $pagePending = combine(
+  $userPending,
+  $cardsPending,
+  (users, cards) => users || cards,
+);
 
 sample({
   source: pageLoaded,
+  fn: ({ params }) => ({
+    body: {
+      username: params.username,
+    },
+  }),
   target: usersGetFx,
 });
 
@@ -27,5 +37,5 @@ sample({
   fn: ({ answer }) => ({
     body: { authorId: answer.user.id },
   }),
-  target: internalApi.cardsList,
+  target: cardsListFx,
 });
