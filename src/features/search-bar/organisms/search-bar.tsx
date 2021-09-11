1e8e23126ecab5b61db250144da1e57ffd8234ab
alexander.sivtsov <alexander.sivtcov@aliexpress.ru>
2021-09-11 14:25:29 +0300

    feature/BOX-217: add user avatar and user name to search-bar

--- src/features/search-bar/organisms/search-bar.tsx
+++ src/features/search-bar/organisms/search-bar.tsx
@@ -1,10 +1,12 @@
 import styled from 'styled-components';
 import React, { useEffect } from 'react';
+import { $currentUser } from '@box/entities/user/model';
 import { Button, ContentCenteredTemplate, IconLogo } from '@box/ui';
 import { Link } from 'react-router-dom';
 import { SessionPanel } from '@box/entities/session';
+import { paths } from '@box/pages/paths';
 import { theme } from '@box/lib/theme';
-import { useEvent } from 'effector-react/ssr';
+import { useEvent, useStore } from 'effector-react/ssr';
 
 import * as model from '../models';
 import { Search } from '../molecules';
@@ -16,12 +18,12 @@ interface SearchbarProps {
   newCardHref: string;
 }
 
-// TODO: вынести из Searchbar логику с пользователем
 export const Searchbar: React.FC<SearchbarProps> = ({
   logoHref,
   newCardHref,
 }) => {
   useSearchQueryChanged();
+  const userInfo = useStore($currentUser);
 
   return (
     <Container>
@@ -33,10 +35,21 @@ export const Searchbar: React.FC<SearchbarProps> = ({
           <SearchWrapper>
             <Search />
           </SearchWrapper>
-          <SessionPanel />
+          {userInfo && (
+            <User to={paths.user(userInfo.username)}>
+              <Avatar>
+                <AvatarImg
+                  src={userInfo.avatar as string}
+                  alt={userInfo.username}
+                />
+              </Avatar>
+              <UserName>{userInfo.username}</UserName>
+            </User>
+          )}
           <NewCardLink to={newCardHref}>
             <Button variant="outlined">New card</Button>
           </NewCardLink>
+          <SessionPanel />
         </Nav>
       </ContentCenteredTemplate>
     </Container>
@@ -71,6 +84,33 @@ const SearchWrapper = styled.div`
   flex-grow: 1;
   margin-left: 3.125rem;
   margin-right: 1.125rem;
+`;
+
+const User = styled(Link)`
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  color: inherit;
+`;
+
+const Avatar = styled.span`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid var(${theme.palette.bnw800});
+  width: 48px;
+  height: 48px;
+  border-radius: 3px;
+  margin-right: 4px;
+`;
+
+const AvatarImg = styled.img`
+  width: 42px;
+  border-radius: 3px;
+`;
+
+const UserName = styled.span`
+  margin-right: 12px;
 `;
 
 const NewCardLink = styled(Link)`
