dba898142c8384970dc23c348f3fe1e28e7ef96c
Kirill Mironov <vetrokm@gmail.com>
2021-11-02 20:53:00 +0300

    fix: Use MultiHeader B3 propagation format

--- src/app/opentelemetry-config.ts
+++ src/app/opentelemetry-config.ts
@@ -1,33 +1,28 @@
-import { B3Propagator } from '@opentelemetry/propagator-b3';
-import {
-  BatchSpanProcessor, ConsoleSpanExporter,
-  SimpleSpanProcessor,
-} from '@opentelemetry/sdk-trace-base';
+import { B3InjectEncoding, B3Propagator } from '@opentelemetry/propagator-b3';
+import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base';
 import { JaegerExporter } from '@opentelemetry/exporter-jaeger';
-import { NodeTracerProvider } from '@opentelemetry/sdk-trace-node';
+import { NodeSDK } from '@opentelemetry/sdk-node';
+import { Resource } from '@opentelemetry/resources';
+import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
 import { TraceIdRatioBasedSampler } from '@opentelemetry/core';
+import { logger } from '@box/shared/lib/logger';
 
-// Configure a tracer provider.
-const provider = new NodeTracerProvider({
-  sampler: new TraceIdRatioBasedSampler(1),
-});
+import pkg from '../../package.json';
 
-// Add a span exporter.
-provider.addSpanProcessor(
-  new BatchSpanProcessor(
+const sdk = new NodeSDK({
+  resource: new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: pkg.name,
+    [SemanticResourceAttributes.SERVICE_VERSION]: pkg.version,
+  }),
+  textMapPropagator: new B3Propagator({
+    injectEncoding: B3InjectEncoding.MULTI_HEADER,
+  }),
+  spanProcessor: new BatchSpanProcessor(
     new JaegerExporter({
       port: Number(process.env.OTEL_EXPORTER_JAEGER_AGENT_PORT!),
     }),
   ),
-);
-
-provider.addSpanProcessor(
-  new SimpleSpanProcessor(
-    new ConsoleSpanExporter(),
-  ),
-);
-
-// Register a global tracer provider.
-provider.register({
-  propagator: new B3Propagator(),
+  sampler: new TraceIdRatioBasedSampler(0.5),
 });
+
+sdk.start().then(() => logger.info('[OPENTELEMETRY] - started'));
